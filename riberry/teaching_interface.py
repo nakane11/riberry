ceb9d8fd81af4ce25ba7324ba86a9b2a95b9a9d6
Aoi <nakane@jsk.imi.i.u-toyko.ac.jp>
2025-01-15 22:52:36 +0900

    add hand special action

--- riberry/teaching_interface.py
+++ riberry/teaching_interface.py
@@ -1,6 +1,15 @@
+import actionlib
 from kxr_controller.kxr_interface import KXRROSRobotInterface
+from hand_v3.msg import HandPoseAction, HandPoseGoal
 
 
 class TeachingRobotInterface(KXRROSRobotInterface):
     def __init__(self, *args, **kwargs):
         super(TeachingRobotInterface, self).__init__(*args, **kwargs)
+        self.hand_client = actionlib.SimpleActionClient('/hand_pose', HandPoseAction)
+        self.hand_client.wait_for_server()
+
+    def send_pose(self, pose):
+        goal = HandPoseGoal(pose=pose)
+        self.hand_client.send_goal(goal)
+        self.hand_client.wait_for_result()
